59eae340aa851eed0c98bf63a29c1b58892f4ea9
dOchojski <daniel.ochojski.25.10@gmail.com>
2025-08-27 21:43:00 +0200

    Initial commit

---
+++ src/main/java/pl/smsvalidator/phishing/SmsValidatorApplication.java
@@ -0,0 +1,13 @@
+package pl.smsvalidator.phishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsValidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/smsvalidator/phishing/SmsValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.smsvalidator.phishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
