5179b4b7c44ec6eda60bf6bd5ac84267c7aa1571
dOchojski <daniel.ochojski.25.10@gmail.com>
2025-09-01 14:10:39 +0200

    sms validator - quickfix

--- src/main/java/pl/smsvalidator/phishing/service/SmsEvaluationService.java
+++ src/main/java/pl/smsvalidator/phishing/service/SmsEvaluationService.java
@@ -1,23 +1,27 @@
 package pl.smsvalidator.phishing.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.smsvalidator.phishing.exception.ExternalServiceException;
 import pl.smsvalidator.phishing.model.Classification;
 import pl.smsvalidator.phishing.model.ConfidenceLevel;
 import pl.smsvalidator.phishing.model.dto.external.EvaluateUriResponse;
-import pl.smsvalidator.phishing.model.dto.internal.*;
-import pl.smsvalidator.phishing.web.EvaluateUriClient;
-
-import java.util.ArrayList;
-import java.util.List;
+import pl.smsvalidator.phishing.model.dto.internal.MessageResultDto;
+import pl.smsvalidator.phishing.model.dto.internal.SmsEvaluationRequest;
+import pl.smsvalidator.phishing.model.dto.internal.SmsEvaluationResponse;
+import pl.smsvalidator.phishing.model.dto.internal.SmsMessageDto;
+import pl.smsvalidator.phishing.model.dto.internal.SmsSubscriptionRequest;
+import pl.smsvalidator.phishing.model.dto.internal.UrlEvaluationDto;
+import pl.smsvalidator.phishing.web.IEvaluateUriClient;
 
 @Service
 @RequiredArgsConstructor
 public class SmsEvaluationService {
 
     private final UrlExtractorService urlExtractorService;
-    private final EvaluateUriClient evaluateUriClient;
+    private final IEvaluateUriClient evaluateUriClient;
     private final SmsSubscriptionService subscriptionService;
 
     public void subscribe(SmsSubscriptionRequest request) {
